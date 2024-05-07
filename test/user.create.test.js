0b47137277d71ff883218d1bf3401e61413d5e99
adsk0165 <a.charkaoui@student.avans.nl>
2024-05-07 09:52:28 +0200

    test

--- test/user.create.test.js
+++ test/user.create.test.js
@@ -22,34 +22,7 @@ describe('UC201 Registreren als nieuwe user', () => {
     /**
      * Hier starten de testcases
      */
-    it('TC-201-1 Verplicht veld ontbreekt', (done) => {
-        chai.request(server)
-            .post(endpointToTest)
-            .send({
-                // firstName: 'Voornaam', ontbreekt
-                lastName: 'Achternaam',
-                emailAdress: 'v.a@server.nl'
-            })
-            .end((err, res) => {
-                /**
-                 * Voorbeeld uitwerking met chai.expect
-                 */
-                chai.expect(res).to.have.status(400)
-                chai.expect(res).not.to.have.status(200)
-                chai.expect(res.body).to.be.a('object')
-                chai.expect(res.body).to.have.property('status').equals(400)
-                chai.expect(res.body)
-                    .to.have.property('message')
-                    .equals('Missing or incorrect firstName field')
-                chai
-                    .expect(res.body)
-                    .to.have.property('data')
-                    .that.is.a('object').that.is.empty
-
-                done()
-            })
-    })
-
+    
     it.skip('TC-201-2 Niet-valide email adres', (done) => {
         done()
     })
